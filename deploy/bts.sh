760acf78ddf22694c43a68b54267168b52d0a713
oobobeo <gkstmdok@yonsei.ac.kr>
2020-11-02 16:37:09 +0900

    .

---
+++ deploy/bts.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+date=$(date +%Y-%m-%d)
+cat << END
+이 문제는 수정될것입니다.
+(This issue will be fixed.)
+(修正されました。)
+
+$date 업데이트된 패치를 적용하세요.
+( Please apply the updated patch of $date )
+(更新されたパッチを適用してください)
+END
