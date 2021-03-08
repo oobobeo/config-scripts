3d51e49d50ffea9a29e4f8277af3001e4ccd4a1e
oobobeo <gkstmdok@yonsei.ac.kr>
2021-03-08 12:40:19 +0900

    .

--- git/git_navigate.sh
+++ git/git_navigate.sh
@@ -9,7 +9,6 @@ while getopts "b" opt;do
   case $opt in
     b)
       b=true
-      echo bbbbbb
       ;;
     *)
       echo 'usage: gn [-b] n'
@@ -19,18 +18,15 @@ done
 shift $((OPTIND-1))
 index=$1
 
-echo b $b
 if [[ -n $b ]];then
-	echo inside if, b = $b
-  index=$((-index))
+  # to older commits
+  git checkout HEAD~$index
+else
+	# to newer commits
+  current_branch=$(git branch --color=never --sort=committerdate | cut -d'*' -f2 | cut -d' ' -f3 | tail -1)
+	target=$(git rev-list --ancestry-path HEAD..$current_branch | tail -$index | head -1)
+  git checkout $target
 fi
 
-echo aa $index
-
-
-
-current_branch=$(git branch --color=never --sort=committerdate | cut -d'*' -f2 | cut -d' ' -f3 | tail -1)
-echo $current_branch
-echo $1
-target=$(git rev-list --ancestry-path HEAD..$current_branch | tail -$1 | head -1)
-echo $target
+# print current commit
+git log --oneline | head -n1
